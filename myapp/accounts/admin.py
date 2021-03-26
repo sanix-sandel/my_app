f1fc59f7a4df84db9540fac2543887ba29ea9778
sanix <sanicksikani@gmail.com>
2021-03-26 08:44:38 +0300

    user models

--- myapp/accounts/admin.py
+++ myapp/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
+
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+
+    form = UserChangeForm
+    model = User
+    list_display = ['email', 'username',]
+
+admin.site.register(User, CustomUserAdmin)    
--- myapp/accounts/tests.py
+++ myapp/accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='sanix',
+            email='sanix@gmail.com',
+            password='sanix2020'
+        )
+        self.assertEqual(user.username, 'sanix')
+        self.assertEqual(user.email, 'sanix@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+
+    def test_create_superuser(self):
+        User = get_user_model()    
+        admin_user = User.objects.create_superuser(
+            username='sanixadmin',
+            email='sanixadmin@gmail.com',
+            password='testadmin123'
+        )
+        self.assertEqual(admin_user.username, 'sanixadmin')
+        self.assertEqual(admin_user.email, 'sanixadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
